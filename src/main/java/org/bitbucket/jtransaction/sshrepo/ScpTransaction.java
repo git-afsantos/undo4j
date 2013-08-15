3d79bfa1704f119dd945d87ef231be93fd73f460
git-afsantos <contact.andre.santos@gmail.com>
2013-08-15 12:12:39 +0200

    SSH transaction - outline

--- src/main/java/org/bitbucket/jtransaction/sshrepo/ScpTransaction.java
+++ src/main/java/org/bitbucket/jtransaction/sshrepo/ScpTransaction.java
@@ -1,28 +1,60 @@
 package org.bitbucket.jtransaction.sshrepo;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.bitbucket.jtransaction.resources.NormalState;
+import org.bitbucket.jtransaction.resources.ResourceState;
 import org.bitbucket.jtransaction.transactions.ManagedResource;
 import org.bitbucket.jtransaction.transactions.TransactionalCallable;
 
 public class ScpTransaction implements TransactionalCallable<Object> {
+	private ManagedResource<String> resource;
+	private String sourceDir, targetDir;
+	
+	public ScpTransaction(ScpResource r) {
+		resource = ManagedResource.from(r);
+	}
+	
 
 	@Override
 	public Object call() throws Exception {
-		// TODO Auto-generated method stub
+		String stampedName = new StringBuilder().
+				append(targetDir).
+				append(System.currentTimeMillis()).toString();
 		
-		/*
-		 * Save timestamp as string
-		 * Rename existing folder on target server, using timestamp
-		 * Copy directory
-		 */
+		renameOldDirectory(stampedName);
 		
-		
+		copyDirectory();
+
 		return null;
 	}
 
 	@Override
 	public Iterable<ManagedResource<?>> getManagedResources() {
-		// TODO Auto-generated method stub
-		return null;
+		List<ManagedResource<?>> list = new LinkedList<>();
+		list.add(resource);
+		return list;
+	}
+	
+	
+	private void renameOldDirectory(String name) {
+		String cmd = new StringBuilder("mv ").
+				append(targetDir).append(' ').
+				append(name).toString();
+		resource.write(stringToState(cmd));
+	}
+	
+	
+	private void copyDirectory() {
+		String cmd = new StringBuilder("scp ").
+				append(sourceDir).append(' ').
+				append(targetDir).toString();
+		resource.write(stringToState(cmd));
 	}
 
+	
+	private ResourceState<String> stringToState(String s) {
+		return new NormalState<String>(s);
+	}
 }
