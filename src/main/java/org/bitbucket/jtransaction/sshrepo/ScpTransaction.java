cf67b51260702029d1d68caec6fa537bab967dcc
VictorCMiraldo <victor.cacciari@gmail.com>
2013-08-15 14:57:40 +0200

    Fixed rollback command.

--- src/main/java/org/bitbucket/jtransaction/sshrepo/ScpTransaction.java
+++ src/main/java/org/bitbucket/jtransaction/sshrepo/ScpTransaction.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.bitbucket.jtransaction.resources.NormalState;
+import org.bitbucket.jtransaction.resources.NullState;
 import org.bitbucket.jtransaction.resources.ResourceState;
 import org.bitbucket.jtransaction.transactions.ManagedResource;
 import org.bitbucket.jtransaction.transactions.TransactionalCallable;
@@ -12,27 +13,26 @@ public class ScpTransaction implements TransactionalCallable<Object> {
 	private ManagedResource<String> resource;
 	private ScpResource scpResource;
 	private String sourceDir, targetDir;
-	
+
 	public ScpTransaction(ScpResource r, String s, String t) {
 		scpResource = r;
 		resource = ManagedResource.from(r);
 		sourceDir = s;
 		targetDir = t;
 	}
-	
 
 	@Override
 	public Object call() throws Exception {
-		String stampedName = new StringBuilder().
-				append(targetDir).
-				append(System.currentTimeMillis()).toString();
-		
+		String stampedName = new StringBuilder().append(targetDir)
+				.append(System.currentTimeMillis()).toString();
+
 		renameRemoteDirectory(stampedName);
-		
+
 		scpResource.setCommitCmd(deleteAfterCommit(stampedName));
 		scpResource.setRollbackCmd(rollbackOnCopying(stampedName));
-		
+
 		copyDirectory();
+		scpResource.setRollbackCmd(new NullState<String>());
 
 		return null;
 	}
@@ -43,44 +43,38 @@ public class ScpTransaction implements TransactionalCallable<Object> {
 		list.add(resource);
 		return list;
 	}
-	
-	
+
 	private void renameRemoteDirectory(String name) {
-		String cmd = new StringBuilder("mv ").
-				append(trimRemoteDirectory(targetDir)).append(' ').
-				append(trimRemoteDirectory(name)).toString();
+		String cmd = new StringBuilder("mv ")
+				.append(trimRemoteDirectory(targetDir)).append(' ')
+				.append(trimRemoteDirectory(name)).toString();
 		resource.write(stringToState(cmd));
 	}
-	
-	
+
 	private void copyDirectory() {
-		String cmd = new StringBuilder("scp -rp ").
-				append(sourceDir).append(' ').
-				append(targetDir).toString();
+		String cmd = new StringBuilder("scp -rp ").append(sourceDir)
+				.append(' ').append(targetDir).toString();
 		resource.write(stringToState(cmd));
 	}
-	
-	
+
 	private ResourceState<String> rollbackOnCopying(String altered) {
-		String cmd = new StringBuilder("rm -rf ").
-				append(trimRemoteDirectory(targetDir)).append("; mv ").
-				append(trimRemoteDirectory(altered)).append(' ').
-				append(trimRemoteDirectory(targetDir)).toString();
-		return stringToState(cmd);
-	}
-	
-	
-	private ResourceState<String> deleteAfterCommit(String altered) {
-		String cmd = new StringBuilder("rm -rf ").
-				append(trimRemoteDirectory(altered)).toString();
+		String cmd = new StringBuilder("rm -rf ")
+				.append(trimRemoteDirectory(targetDir)).append("; mv ")
+				.append(trimRemoteDirectory(altered)).append(' ')
+				.append(trimRemoteDirectory(targetDir)).toString();
 		return stringToState(cmd);
 	}
 
-	
+	private ResourceState<String> deleteAfterCommit(String altered) {
+		String cmd = new StringBuilder("rm -rf ").append(
+				trimRemoteDirectory(altered)).toString();
+		return stringToState(cmd);
+	}
+
 	private ResourceState<String> stringToState(String s) {
 		return new NormalState<String>(s);
 	}
-	
+
 	private String trimRemoteDirectory(String s) {
 		int i = s.indexOf(':');
 		if (i >= 0 && s.length() > i + 1) {
