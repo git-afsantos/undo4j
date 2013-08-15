cae26013fa67eb402f1eefdb7aee0fc5b2ef6f9e
VictorCMiraldo <victor.cacciari@gmail.com>
2013-08-15 11:55:35 +0200

    CommandIssuer partially done.

--- src/main/java/org/bitbucket/jtransaction/sshrepo/CommandIssuer.java
+++ src/main/java/org/bitbucket/jtransaction/sshrepo/CommandIssuer.java
@@ -3,7 +3,29 @@ package org.bitbucket.jtransaction.sshrepo;
 import org.bitbucket.jtransaction.resources.InternalResource;
 import org.bitbucket.jtransaction.resources.ResourceState;
 
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+
 public final class CommandIssuer implements InternalResource<String> {
+	public static class CommandIssuerException extends Exception {
+		public CommandIssuerException(String msg) {
+			super(msg);
+		}
+	}
+
+	private ChannelExec channel;
+
+	/*
+	 * Receives an active (connected) session to create an ChannelExec.
+	 */
+	public CommandIssuer(Session active) throws CommandIssuerException,
+			JSchException {
+		if (!active.isConnected())
+			throw new CommandIssuerException("Must receive an active session");
+
+		this.channel = (ChannelExec) active.openChannel("exec");
+	}
 
 	@Override
 	public boolean isValidState(ResourceState<String> state) {
@@ -13,14 +35,11 @@ public final class CommandIssuer implements InternalResource<String> {
 
 	@Override
 	public ResourceState<String> buildState() throws Exception {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void applyState(ResourceState<String> state) throws Exception {
-		// Applies the given command on ssh-exec
-
+		channel.setCommand(state.get());
 	}
-
 }
