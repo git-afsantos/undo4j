78b03b52049a4321e212eafae4d7de7aa18469ff
Miguel Ferreira <miguelferreira@me.com>
2013-08-20 19:06:17 +0200

    Add constructor that does not need a lock manager. Null lock manger will be used by default

--- src/main/java/com/github/undo4j/resources/ShadowResource.java
+++ src/main/java/com/github/undo4j/resources/ShadowResource.java
@@ -1,167 +1,167 @@
 package com.github.undo4j.resources;
 
-import com.github.undo4j.common.LockManager;
+import static com.github.undo4j.resources.StateUtil.cloneSafely;
+import static com.github.undo4j.resources.StateUtil.identity;
 
-import static com.github.undo4j.resources.StateUtil.*;
+import com.github.undo4j.common.LockManager;
+import com.github.undo4j.common.LockManagers;
 
 /**
- * ShadowResource is not thread-safe.
- * Implements a resource with shadow writes.
+ * ShadowResource is not thread-safe. Implements a resource with shadow writes.
  * 
  * @author afs
  * @version 2013
-*/
+ */
 
 public final class ShadowResource<T> extends SingleWriterStatefulResource<T> {
-    // instance variables
-    private ResourceState<T> shadow;
+	// instance variables
+	private ResourceState<T> shadow;
 
-    /**************************************************************************
-     * Constructors
-    **************************************************************************/
+	/**************************************************************************
+	 * Constructors
+	 **************************************************************************/
 
-    /** Parameter constructor of objects of class ShadowResource. */
-    public ShadowResource(
-		InternalResource<T> resource, LockManager lockManager
-	) {
-        super(resource, lockManager);
-        this.shadow = getCheckpointReference();
-    }
+	/** Parameter constructor of objects of class ShadowResource. */
+	public ShadowResource(InternalResource<T> resource, LockManager lockManager) {
+		super(resource, lockManager);
+		this.shadow = getCheckpointReference();
+	}
 
+	public ShadowResource(InternalResource<T> resource) {
+		super(resource, LockManagers.newNullLockManager());
+		this.shadow = getCheckpointReference();
+	}
 
-    /** Copy constructor of objects of class ShadowResource. */
-    protected ShadowResource(ShadowResource<T> instance) {
-        super(instance);
-        this.shadow = instance.getShadow();
-    }
+	/** Copy constructor of objects of class ShadowResource. */
+	protected ShadowResource(ShadowResource<T> instance) {
+		super(instance);
+		this.shadow = instance.getShadow();
+	}
 
+	/**************************************************************************
+	 * Getters
+	 **************************************************************************/
 
+	/** */
+	protected final ResourceState<T> getShadow() {
+		return cloneSafely(this.shadow);
+	}
 
-    /**************************************************************************
-     * Getters
-    **************************************************************************/
+	/** */
+	protected final ResourceState<T> getShadowReference() {
+		return this.shadow;
+	}
 
-    /** */
-    protected final ResourceState<T> getShadow() {
-        return cloneSafely(this.shadow);
-    }
+	/**************************************************************************
+	 * Setters
+	 **************************************************************************/
 
-    /** */
-    protected final ResourceState<T> getShadowReference() {
-        return this.shadow;
-    }
+	/** */
+	protected final void setShadow(ResourceState<T> state) {
+		this.shadow = identity(state);
+	}
 
+	/**************************************************************************
+	 * Predicates
+	 **************************************************************************/
 
+	/** */
+	protected final boolean hasShadow() {
+		return !this.shadow.isNull();
+	}
 
-    /**************************************************************************
-     * Setters
-    **************************************************************************/
+	/**************************************************************************
+	 * Public Methods
+	 **************************************************************************/
 
-    /** */
-    protected final void setShadow(ResourceState<T> state) {
-        this.shadow = identity(state);
-    }
+	/**
+	 * Stores the given state, as a shadow. The effective write happens on
+	 * commit, applying the shadow state. This method assumes the resource has
+	 * been acquired.
+	 */
+	@Override
+	public final void write(ResourceState<T> state) {
+		setShadow(state);
+		setStatus(Status.CHANGED);
+	}
 
+	/**
+	 * Attempts to commit the changes made on this resource, invoking applyState
+	 * on the internal resource. Also discards the stored shadow, after
+	 * committing. Throws a ResourceCommitException, if any exception occurs,
+	 * while applying the changes. This method assumes the resource has been
+	 * acquired.
+	 */
+	@Override
+	public final void commit() {
+		// If there's a shadow, apply it, update local commit, and discard it.
+		if (isChanged()) {
+			if (hasShadow()) {
+				applyShadow();
+				// A clone of the shadow is set as the local commit.
+				// No writer may alter the referenced object after commit.
+				setLocalCommit(this.shadow.clone());
+				this.shadow = identity(null);
+			}
+			setStatus(Status.COMMITTED);
+			setConsistent(true);
+		}
+	}
 
+	/**
+	 * Reverts the changes made on this resource. Throws a
+	 * ResourceRollbackException, if any exception occurs while reverting the
+	 * changes. This method does not distinguish between readers and writers.
+	 * Such distinction (for instance, rollback behaving as an identity for
+	 * readers) is beyond the scope of this class. This method assumes the
+	 * resource has been acquired.
+	 */
+	@Override
+	public final void rollback() {
+		switch (getStatus()) {
+		case COMMITTED:
+			if (hasLocalCommit()) {
+				rollbackToCheckpoint();
+			}
+			break;
 
-    /**************************************************************************
-     * Predicates
-    **************************************************************************/
+		case UPDATED:
+			if (hasDifferentPreviousCheckpoint()) {
+				rollbackToPrevious();
+			}
+			break;
 
-    /** */
-    protected final boolean hasShadow() { return !this.shadow.isNull(); }
+		default:
+			break;
+		}
+		// Discard the shadow and the local commit.
+		this.shadow = identity(null);
+		setLocalCommit(this.shadow);
+		setStatus(Status.UPDATED);
+		setConsistent(true);
+	}
 
+	/**************************************************************************
+	 * Private Methods
+	 **************************************************************************/
 
+	/** Validate any changes made, by applying the current shadow. */
+	private void applyShadow() {
+		try {
+			getInternalResource().applyState(this.shadow);
+		} catch (Exception e) {
+			setConsistent(false);
+			throw new ResourceCommitException(e.getMessage(), e);
+		}
+	}
 
-    /**************************************************************************
-     * Public Methods
-    **************************************************************************/
+	/**************************************************************************
+	 * Clone
+	 **************************************************************************/
 
-    /** Stores the given state, as a shadow.
-     * The effective write happens on commit, applying the shadow state.
-     * This method assumes the resource has been acquired.
-     */
-    @Override
-    public final void write(ResourceState<T> state) {
-        setShadow(state);
-        setStatus(Status.CHANGED);
-    }
-
-
-    /** Attempts to commit the changes made on this resource,
-     * invoking applyState on the internal resource.
-     * Also discards the stored shadow, after committing.
-     * Throws a ResourceCommitException, if any exception occurs,
-     * while applying the changes.
-     * This method assumes the resource has been acquired.
-     */
-    @Override
-    public final void commit() {
-        // If there's a shadow, apply it, update local commit, and discard it.
-        if (isChanged()) {
-            if (hasShadow()) {
-                applyShadow();
-                // A clone of the shadow is set as the local commit.
-                // No writer may alter the referenced object after commit.
-                setLocalCommit(this.shadow.clone());
-                this.shadow = identity(null);
-            }
-            setStatus(Status.COMMITTED);
-            setConsistent(true);
-        }
-    }
-
-
-    /** Reverts the changes made on this resource.
-     * Throws a ResourceRollbackException, if any exception occurs
-     * while reverting the changes.
-     * This method does not distinguish between readers and writers.
-     * Such distinction (for instance, rollback behaving as an identity
-     * for readers) is beyond the scope of this class.
-     * This method assumes the resource has been acquired.
-     */
-    @Override
-    public final void rollback() {
-        switch (getStatus()) {
-            case COMMITTED:
-            if (hasLocalCommit()) { rollbackToCheckpoint(); }
-            break;
-            
-            case UPDATED:
-            if (hasDifferentPreviousCheckpoint()) { rollbackToPrevious(); }
-            break;
-            
-            default: break;
-        }
-        // Discard the shadow and the local commit.
-        this.shadow = identity(null);
-        setLocalCommit(this.shadow);
-        setStatus(Status.UPDATED);
-        setConsistent(true);
-    }
-
-
-
-    /**************************************************************************
-     * Private Methods
-    **************************************************************************/
-
-    /** Validate any changes made, by applying the current shadow. */
-    private void applyShadow() {
-        try { getInternalResource().applyState(this.shadow); }
-        catch (Exception e) {
-            setConsistent(false);
-            throw new ResourceCommitException(e.getMessage(), e);
-        }
-    }
-
-
-
-    /**************************************************************************
-     * Clone
-    **************************************************************************/
-
-    /** Creates and returns a (deep) copy of this object. */
-    @Override
-    public ShadowResource<T> clone() { return new ShadowResource<T>(this); }
+	/** Creates and returns a (deep) copy of this object. */
+	@Override
+	public ShadowResource<T> clone() {
+		return new ShadowResource<T>(this);
+	}
 }
